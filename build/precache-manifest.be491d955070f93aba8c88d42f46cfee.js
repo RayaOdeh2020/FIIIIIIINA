d8b8524df777904c2dd14a157bf8941094a4f19e
RayaOdeh2020 <74620387+RayaOdeh2020@users.noreply.github.com>
2020-11-17 20:50:56 +0200

    Add files via upload

---
+++ build/precache-manifest.be491d955070f93aba8c88d42f46cfee.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e01667e12655a0d2eebf2502ee4fc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c86fec3b680aea7896e0",
+    "url": "/static/css/2.a01496f9.chunk.css"
+  },
+  {
+    "revision": "9fdd34136d626ea2ef9a",
+    "url": "/static/css/main.c24f4ba4.chunk.css"
+  },
+  {
+    "revision": "c86fec3b680aea7896e0",
+    "url": "/static/js/2.0f40752f.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.0f40752f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fdd34136d626ea2ef9a",
+    "url": "/static/js/main.3d31b616.chunk.js"
+  },
+  {
+    "revision": "f4f4d02fd2e7b6f5c768",
+    "url": "/static/js/runtime-main.ccde8b7b.js"
+  },
+  {
+    "revision": "8532dcab37354e6c693df1cc19058610",
+    "url": "/static/media/AI.8532dcab.jpg"
+  },
+  {
+    "revision": "5d6fdf9732042cea0e97348e0f0177a6",
+    "url": "/static/media/Android.5d6fdf97.jpg"
+  },
+  {
+    "revision": "912c3e9e346c170565a9b4c49485fca1",
+    "url": "/static/media/ML.912c3e9e.jpg"
+  },
+  {
+    "revision": "5a147418d33fc2b7c46b04381df53665",
+    "url": "/static/media/Web.5a147418.jpg"
+  },
+  {
+    "revision": "cbb4eddb5fd112a59650b3e5fcf6d12f",
+    "url": "/static/media/computer-1245714_1920.cbb4eddb.jpg"
+  },
+  {
+    "revision": "13ca235315db4417920fb51fba1380c4",
+    "url": "/static/media/contact.13ca2353.svg"
+  },
+  {
+    "revision": "0ccb8d658fb6d8a1bd116aefcc17cc68",
+    "url": "/static/media/photo2.0ccb8d65.svg"
+  },
+  {
+    "revision": "876eb8befc9a36170258fdef9cd4b5e9",
+    "url": "/static/media/startup-593327_1920.876eb8be.jpg"
+  },
+  {
+    "revision": "05a0df7e0a1f218bbe44f3296c68686e",
+    "url": "/static/media/student-849825_1920.05a0df7e.jpg"
+  },
+  {
+    "revision": "bf78a4c722af8668059b09dc0fb01fb0",
+    "url": "/static/media/work-731198_1920.bf78a4c7.jpg"
+  }
+]);
